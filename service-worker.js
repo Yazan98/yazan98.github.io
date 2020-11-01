7448d478cc016428a528a6d19e66ae3b998041ad
DESKTOP-6JRHMBT\yazan <yazantarifi98@gmail.com>
2020-10-31 17:33:13 -0700

    Fix Some Pages

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.42f51bec0f088d63c7251c545d6ffdee.js"
+  "/precache-manifest.1195ca5cb603787b89ccf21175b6c99a.js"
 );
 
 self.addEventListener('message', (event) => {
